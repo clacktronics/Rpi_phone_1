12ff925d7a8f0707a527e2612a88411a3e125db2
clacktronics <ben@clacktronics.co.uk>
2017-02-19 12:32:50 +0000

    Not working but almost there, files play loop, new playback style

--- phone.py
+++ phone.py
@@ -1,38 +1,54 @@
-import RPi.GPIO as GPIO
-from omxplayer import OMXPlayer as omx
-import sys
+import RPi.GPIO as GPIO 
+from omxplayer import OMXPlayer as omx 
 from time import sleep
+import sys,os
+
+accepted_files = ['wav','mp3','m4a','mp4','aif'] 
+
+omxargs = sys.argv
+
+files = [elem for elem in os.listdir('files') if elem[0] != '.']
+files = [elem for elem in files if elem[-3:] in accepted_files]
+print files
+number_files = len(files)
 
 
-vid = sys.argv[0]
-omxargs = sys.argv[1:]
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-try:
-
-    video = omx(vid, args = omxargs, pause=True)
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-
-    HOOK = 4
-    GPIO.setup(HOOK, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(HOOK, GPIO.RISING)
-
-    already_played = False
-
-    while True:
-        if GPIO.event_detected(HOOK) and GPIO.input(HOOK):
-            video.pause()
-            video.set_position(0)
-            already_played = False
-            print "hold - Receiver Down"
-
-        if not already_played and not GPIO.input(HOOK):
-            video.play()
-            already_played = True
-            print "play - Receiver up"
+HOOK = 4
+GPIO.setup(HOOK, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(HOOK, GPIO.FALLING)
 
 
-except KeyboardInterrupt:
-    video.stop()
-    print "Bye"    
+while True:
+    try:
+        for file in files:
+            otpt = 'files/%s' % file
+            video = omx('korn.mp4', args = omxargs, pause=False)        
+            plays = 0
+            played = False
+
+            while plays < 1:
+                if GPIO.event_detected(HOOK) and not GPIO.input(HOOK):
+                    video.play()
+                    print "Playing %s" % file
+                    played = True
+
+                if played and GPIO.input(HOOK):
+                    plays +=1
+                    already_played = True
+                    print "receiver down"
+
+            print "Next track"
+            video.stop()
+            del video
+            sleep(1)
+
+    except KeyboardInterrupt:
+        video.stop()
+        print "Bye"
+        break    
+
+    except Exception as e:
+        print e
